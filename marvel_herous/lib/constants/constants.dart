2e03e263dbef4577c0e3db15c9073856cdffbccb
Nariman Nazarov <nazarov.nh@gmail.com>
2022-10-18 23:44:48 +0600

    feat: added new herous

--- marvel_herous/lib/constants/constants.dart
+++ marvel_herous/lib/constants/constants.dart
@@ -6,17 +6,37 @@ const double WIDTH_CARD = 300;
 const deadPool = 'Deadpool';
 const ironMan = 'Iron Man';
 const spiderMan = 'Sprider Man';
+const doctorStrange = 'Doctor Strange';
+const hulk = 'Hulk';
+const thor = 'Thor';
+const thanos = 'Thanos';
 
-const List<String> listHerous = [deadPool, ironMan, spiderMan];
+const List<String> listHerous = [
+  deadPool,
+  ironMan,
+  spiderMan,
+  doctorStrange,
+  hulk,
+  thor,
+  thanos
+];
 
 const Map<String, String> herousImage = {
   deadPool: 'assets/herous/deadpool.png',
   ironMan: 'assets/herous/iron_man.png',
-  spiderMan: 'assets/herous/spiderman.jpg'
+  spiderMan: 'assets/herous/spiderman.jpg',
+  doctorStrange: 'assets/herous/doctor_strange.jpg',
+  hulk: 'assets/herous/hulk.jpg',
+  thor: 'assets/herous/thor.webp',
+  thanos: 'assets/herous/thanos.jpg',
 };
 
 const Map<int, Color> herousColors = {
   0: Color.fromARGB(252, 126, 26, 24),
   1: Color.fromARGB(255, 202, 187, 49),
-  2: Colors.blue
+  2: Colors.blue,
+  3: Color.fromARGB(255, 32, 230, 220),
+  4: Colors.green,
+  5: Colors.blue,
+  6: Colors.purple
 };
